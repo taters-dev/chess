2d4db54cdcfe9f136af34f9f50747a909f75915c
tate mccoy <novatate45@gmail.com>
2026-02-19 22:06:21 -0700

    Getting started Phase 3: Chess Web API

---
+++ server/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import chess.*;
+import server.Server;
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run(8080);
+
+        System.out.println("♕ 240 Chess Server");
+    }
+}
